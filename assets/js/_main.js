70413ff25754c8a3ff71da58c63bee0078f704e0
Dmitro Sekhno <muravytskyi.github.io>
2020-07-06 11:23:46 +0300

    animation, html, css

---
+++ assets/js/_main.js
@@ -0,0 +1,103 @@
+window.addEventListener('DOMContentLoaded', function () {
+    console.log('DOMContentLoaded');
+
+    var video = document.getElementById('video'),
+        canvas = document.getElementById('canvas'),
+        // enter = document.getElementById('enter'),
+        buttons = document.querySelectorAll('.buttons'),
+        promise = video.play(),
+        logo = document.querySelector('.logo'),
+        easeInOut = 'power3.out',
+        logoAnim = gsap.timeline({
+            ease: easeInOut
+        }),
+        buttonsAnim = gsap.timeline({
+            ease: easeInOut
+        });
+
+
+    console.log(video.getBoundingClientRect());
+
+    if (promise !== undefined) {
+        promise.then(_ => {
+            // Autoplay started!
+            console.log('Autoplay started!');
+            animLogo();
+            paintVideo();
+            animButtons();
+        }).catch(error => {
+            console.log(error);
+            // Autoplay was prevented.
+            // Show a "Play" button so that user can start playback.
+            animLogo();
+            animButtons();
+        });
+    }
+
+    buttonsAnim.set(buttons, {
+        opacity: 0
+    });
+
+    video.addEventListener('ended', function listener() {
+        console.log('Ended!')
+        this.currentTime = 0;
+        this.loop = true;
+        this.play();
+        this.removeEventListener('ended', listener, false);
+    }, false);
+
+
+
+    function paintVideo() {
+        if (canvas) {
+            canvas.width = video.videoWidth = window.innerWidth;
+            canvas.height = video.videoHeight = window.innerHeight;
+            canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+            if (!video.paused) {
+                requestAnimationFrame(paintVideo);
+            }
+        }
+    }
+
+    function animLogo() {
+        console.log('animation logo!!!');
+
+
+
+        // logoAnim.from(logo, video.duration, {
+        //     opacity: 0,
+        //     scale: 0.4,
+        //     ease: easeInOut,
+        //     delay: -1,
+        //     rotationX: 270,
+        //     transformOrigin: 'bottom',
+        //     transformStyle: 'preserve-3d'
+        // });
+        logoAnim.fromTo(
+            logo, {
+                opacity: 0,
+                scale: 0.4
+            }, {
+                opacity: 1,
+                scale: 1,
+                duration: video.duration,
+                ease: easeInOut
+            }
+        );
+    }
+
+    function animButtons() {
+        console.log('animation buttons');
+        buttonsAnim.fromTo(
+            buttons, {
+                opacity: 0,
+            }, {
+                opacity: 1,
+                duration: .3,
+                delay: 6,
+                ease: easeInOut
+            }
+        );
+    }
+
+})
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,103 +1,40 @@
-window.addEventListener('DOMContentLoaded', function(){
-    console.log('DOMContentLoaded');
-
-    var video = document.getElementById('video'),
-        canvas = document.getElementById('canvas'),
-        // enter = document.getElementById('enter'),
-        buttons = document.querySelectorAll('.buttons'),
-        promise = video.play(),
-        logo = document.querySelector('.logo'),
-        easeInOut = 'power3.out',
-        logoAnim = gsap.timeline({
-            ease: easeInOut
-        }),
-        buttonsAnim = gsap.timeline({
-            ease: easeInOut
-        });
-
+(function(){
     
-    console.log(video.getBoundingClientRect());
-
-    if (promise !== undefined) {
-        promise.then(_ => {
-            // Autoplay started!
-            console.log('Autoplay started!');
-            animLogo();
-            paintVideo();
-            animButtons();
-        }).catch(error => {
-            console.log(error);
-            // Autoplay was prevented.
-            // Show a "Play" button so that user can start playback.
-            animLogo();
-            animButtons();
-        });
-    }
-
-    buttonsAnim.set(buttons, {
-        opacity: 0
+    var canvasVideo = new CanvasVideoPlayer({
+        videoSelector: '.video',
+        canvasSelector: '.canvas',
+        timelineSelector: false,
+        autoplay: true,
+        makeLoop: true,
+        pauseOnClick: false,
+        audio: false
     });
 
-    video.addEventListener('ended', function listener() {
-        console.log('Ended!')
-        this.currentTime = 0;
-        this.loop = true;
-        this.play();
-        this.removeEventListener('ended', listener, false);
-    }, false);
+    canvasVideo.play();
+
+    var buttons = document.querySelector('.wrapper-buttons'),
+        logo = document.querySelector('.wrapper-logo'),
+        easeInOut = 'power3.out',
+        timeline = gsap.timeline({
+            ease: easeInOut
+        });
+
+        timeline.set(buttons, {
+            opacity: 0
+        });
+
+        timeline.from(logo, 6, {
+            opacity: 0,
+            scale: 0,
+            ease: easeInOut,
+            delay: -1,
+            rotationX: 270,
+            transformOrigin: 'center',
+            transformStyle: 'preserve-3d'
+        }).then(function(){
+            timeline.to(buttons, .2, {
+                opacity: 1
+            })
+        });
     
-    
-
-    function paintVideo() {
-        if (canvas) {
-            canvas.width = video.videoWidth = window.innerWidth;
-            canvas.height = video.videoHeight = window.innerHeight;
-            canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-            if (!video.paused) {
-                requestAnimationFrame(paintVideo);
-            }
-        }
-    }
-
-    function animLogo() {
-        console.log('animation logo!!!');
-
-        
-
-        // logoAnim.from(logo, video.duration, {
-        //     opacity: 0,
-        //     scale: 0.4,
-        //     ease: easeInOut,
-        //     delay: -1,
-        //     rotationX: 270,
-        //     transformOrigin: 'bottom',
-        //     transformStyle: 'preserve-3d'
-        // });
-        logoAnim.fromTo(
-            logo, {
-                opacity: 0,
-                scale: 0.4
-            }, {
-                opacity: 1,
-                scale: 1,
-                duration: video.duration,
-                ease: easeInOut
-            }
-        );
-    }
-
-    function animButtons() {
-        console.log('animation buttons');
-        buttonsAnim.fromTo(
-            buttons, {
-                opacity: 0,
-            }, {
-                opacity: 1,
-                duration: .3,
-                delay: 6,
-                ease: easeInOut
-            }
-        );
-    }
-    
-})
+}());
