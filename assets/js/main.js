93e460754dc0eed02a2b52825cc13e80ed26a0fc
Dmitro Sekhno <muravytskyi.github.io>
2020-07-09 17:04:40 +0300

    css animation svg

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,33 +11,4 @@
     });
 
     canvasVideo.play();
-
-    var buttons = document.querySelector('.wrapper-buttons'),
-        logo = document.querySelector('.wrapper-logo'),
-        easeInOut = 'power3.out',
-        timeline = gsap.timeline({
-            ease: easeInOut
-        });
-
-        timeline.set(buttons, {
-            opacity: 0
-        });
-
-        timeline.set(logo, {
-            opacity: 0
-        })
-
-        timeline.from(logo, 4, {
-            opacity: 0,
-            scale: 0.4,
-            ease: easeInOut,
-            delay: 1,
-            transformOrigin: 'center',
-            transformStyle: 'preserve-3d'
-        }).then(function(){
-            timeline.to(buttons, .2, {
-                opacity: 1
-            });
-        });
-    
 }());
